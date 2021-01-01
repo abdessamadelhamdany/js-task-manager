05eafb9522a148983b0031c683a431016431c40b
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-01-01 22:19:56 +0100

    Hash user password on create and update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/js-task-manage-api', {
   useCreateIndex: true,
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true
 })
