c25afd290a0aec73540b678d465ba596317f5551
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-01-03 13:53:45 +0100

    Setup Jest and Supertest and write simple test suite for user

---
+++ tests/user.test.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose')
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+  _id: userOneId,
+  name: 'Mike',
+  email: 'mike@example.com',
+  password: '56what!!',
+  tokens: [
+    {
+      token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }
+  ]
+}
+
+beforeAll(async () => {
+  require('../src/db/mongoose')
+})
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'Abdessamad Thow',
+      email: 'abdessamad@example.co',
+      password: 'Frefd123!$'
+    })
+    .expect(201)
+})
+
+test('Should login existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({email: userOne.email, password: 'invalidpass'})
+    .expect(400)
+})
+
+test('Should get profile for user', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+  await request(app).get('/users/me').send().expect(401)
+})
+
+test('Should delete account for user', async () => {
+  await request(app)
+    .delete('users/me')
+    .set('Authorization', `bearer ${userOne.tokens[0].token}`)
+  expect(200)
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+  await request(app).delete('/users/me').expect(401)
+})
